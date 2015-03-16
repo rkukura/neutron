f94a5d7f913bda6cdb41e6ccb932bd4876b58117
Robert Kukura <kukura@noironetworks.com>
2015-03-15 23:38:26 -0400

    implement binding result and levels db functions
    Change-Id: Iee0cc588fd7e8b4c7be26f4d25742c179aa6175f

--- neutron/tests/unit/plugins/ml2/test_db.py
+++ neutron/tests/unit/plugins/ml2/test_db.py
@@ -220,136 +220,137 @@ class Ml2DBTestCase(testlib_api.SqlTestCase):
         self.assertIsNone(port)
         self.assertIsNone(binding)
 
+    def test_binding_result(self):
+        network_id = uuidutils.generate_uuid()
+        port_id = uuidutils.generate_uuid()
+        host = 'test_host'
+        vif_type = 'test_vif_type'
+        vif_details = 'test_vif_details'
+        segment = {api.NETWORK_TYPE: 'vlan',
+                   api.PHYSICAL_NETWORK: 'physnet1',
+                   api.SEGMENTATION_ID: 1}
 
-class Ml2DvrDBTestCase(testlib_api.SqlTestCase):
+        self._setup_neutron_network(network_id)
+        ml2_db.add_network_segment(self.ctx.session,
+                                   network_id,
+                                   segment)
+        segments = ml2_db.get_network_segments(self.ctx.session,
+                                               network_id)
+        segment_id = segments[0][api.ID]
 
-    def setUp(self):
-        super(Ml2DvrDBTestCase, self).setUp()
-        self.ctx = context.get_admin_context()
+        self._setup_neutron_port(network_id, port_id)
 
-    def _setup_neutron_network(self, network_id, port_ids):
+        level0 = models.PortBindingLevel(port_id=port_id,
+                                         host=host,
+                                         level=0,
+                                         driver='test_driver_0',
+                                         segment_id=segment_id)
+        level1 = models.PortBindingLevel(port_id=port_id,
+                                         host=host,
+                                         level=1,
+                                         driver='test_driver_1',
+                                         segment_id=segment_id)
+        levels = [level0, level1]
+
+        # Verify no binding result.
         with self.ctx.session.begin(subtransactions=True):
-            self.ctx.session.add(models_v2.Network(id=network_id))
-            ports = []
-            for port_id in port_ids:
-                mac_address = (db_base_plugin_v2.NeutronDbPluginV2.
-                               _generate_mac())
-                port = models_v2.Port(id=port_id,
-                                      network_id=network_id,
-                                      mac_address=mac_address,
-                                      admin_state_up=True,
-                                      status='ACTIVE',
-                                      device_id='',
-                                      device_owner='')
-                self.ctx.session.add(port)
-                ports.append(port)
-            return ports
+            result = ml2_db.get_binding_result(self.ctx.session,
+                                               port_id,
+                                               host)
+            self.assertIsNone(result)
 
-    def _setup_neutron_router(self):
+        # Verify no binding levels.
         with self.ctx.session.begin(subtransactions=True):
-            router = l3_db.Router()
-            self.ctx.session.add(router)
-            return router
+            result_levels = ml2_db.get_binding_levels(self.ctx.session,
+                                                      port_id,
+                                                      host)
+            self.assertEqual(0, len(result_levels))
 
-    def _setup_dvr_binding(self, network_id, port_id, router_id, host_id):
+        # Set binding result.
         with self.ctx.session.begin(subtransactions=True):
-            record = models.DVRPortBinding(
-                port_id=port_id,
-                host=host_id,
-                router_id=router_id,
-                vif_type=portbindings.VIF_TYPE_UNBOUND,
-                vnic_type=portbindings.VNIC_NORMAL,
-                status='DOWN')
-            self.ctx.session.add(record)
-            return record
+            result = ml2_db.set_binding_result(self.ctx.session,
+                                               port_id,
+                                               host,
+                                               vif_type,
+                                               vif_details,
+                                               levels)
+            self.assertEqual(port_id, result.port_id)
+            self.assertEqual(host, result.host)
+            self.assertEqual(vif_type, result.vif_type)
+            self.assertEqual(vif_details, result.vif_details)
 
-    def test_ensure_dvr_port_binding_deals_with_db_duplicate(self):
-        network_id = 'foo_network_id'
-        port_id = 'foo_port_id'
-        router_id = 'foo_router_id'
-        host_id = 'foo_host_id'
-        self._setup_neutron_network(network_id, [port_id])
-        self._setup_dvr_binding(network_id, port_id, router_id, host_id)
-        with mock.patch.object(query.Query, 'first') as query_first:
-            query_first.return_value = []
-            with mock.patch.object(ml2_db.LOG, 'debug') as log_trace:
-                binding = ml2_db.ensure_dvr_port_binding(
-                    self.ctx.session, port_id, host_id, router_id)
-        self.assertTrue(query_first.called)
-        self.assertTrue(log_trace.called)
-        self.assertEqual(port_id, binding.port_id)
+        # Get and verify binding result.
+        with self.ctx.session.begin(subtransactions=True):
+            result = ml2_db.get_binding_result(self.ctx.session,
+                                               port_id,
+                                               host)
+            self.assertEqual(port_id, result.port_id)
+            self.assertEqual(host, result.host)
+            self.assertEqual(vif_type, result.vif_type)
+            self.assertEqual(vif_details, result.vif_details)
 
-    def test_ensure_dvr_port_binding(self):
-        network_id = 'foo_network_id'
-        port_id = 'foo_port_id'
-        self._setup_neutron_network(network_id, [port_id])
-        router = self._setup_neutron_router()
-        ml2_db.ensure_dvr_port_binding(
-            self.ctx.session, port_id, 'foo_host', router.id)
-        expected = (self.ctx.session.query(models.DVRPortBinding).
-                    filter_by(port_id=port_id).one())
-        self.assertEqual(expected.port_id, port_id)
+        # Get and verify binding levels.
+        with self.ctx.session.begin(subtransactions=True):
+            result_levels = ml2_db.get_binding_levels(self.ctx.session,
+                                                      port_id,
+                                                      host)
+            self.assertEqual(2, len(result_levels))
+            self.assertEqual(level0, result_levels[0])
+            self.assertEqual(level1, result_levels[1])
 
-    def test_ensure_dvr_port_binding_multiple_bindings(self):
-        network_id = 'foo_network_id'
-        port_id = 'foo_port_id'
-        self._setup_neutron_network(network_id, [port_id])
-        router = self._setup_neutron_router()
-        ml2_db.ensure_dvr_port_binding(
-            self.ctx.session, port_id, 'foo_host_1', router.id)
-        ml2_db.ensure_dvr_port_binding(
-            self.ctx.session, port_id, 'foo_host_2', router.id)
-        bindings = (self.ctx.session.query(models.DVRPortBinding).
-                    filter_by(port_id=port_id).all())
-        self.assertEqual(2, len(bindings))
+        # Clear binding result.
+        with self.ctx.session.begin(subtransactions=True):
+            ml2_db.clear_binding_result(self.ctx.session,
+                                        port_id,
+                                        host)
 
-    def test_delete_dvr_port_binding(self):
-        network_id = 'foo_network_id'
-        port_id = 'foo_port_id'
-        self._setup_neutron_network(network_id, [port_id])
-        router = self._setup_neutron_router()
-        binding = self._setup_dvr_binding(
-            network_id, port_id, router.id, 'foo_host_id')
-        ml2_db.delete_dvr_port_binding(
-            self.ctx.session, port_id, 'foo_host_id')
-        count = (self.ctx.session.query(models.DVRPortBinding).
-            filter_by(port_id=binding.port_id).count())
-        self.assertFalse(count)
+        # Verify no binding result.
+        with self.ctx.session.begin(subtransactions=True):
+            result = ml2_db.get_binding_result(self.ctx.session,
+                                               port_id,
+                                               host)
+            self.assertIsNone(result)
 
-    def test_delete_dvr_port_binding_not_found(self):
-        ml2_db.delete_dvr_port_binding(
-            self.ctx.session, 'foo_port_id', 'foo_host')
+        # Verify no binding levels.
+        with self.ctx.session.begin(subtransactions=True):
+            result_levels = ml2_db.get_binding_levels(self.ctx.session,
+                                                      port_id,
+                                                      host)
+            self.assertEqual(0, len(result_levels))
 
-    def test_delete_dvr_port_binding_if_stale(self):
-        network_id = 'foo_network_id'
-        port_id = 'foo_port_id'
-        self._setup_neutron_network(network_id, [port_id])
-        binding = self._setup_dvr_binding(
-            network_id, port_id, None, 'foo_host_id')
+    def test_binding_result_no_levels(self):
+        network_id = uuidutils.generate_uuid()
+        port_id = uuidutils.generate_uuid()
+        host = 'test_host'
+        vif_type = 'test_vif_type'
 
-        ml2_db.delete_dvr_port_binding_if_stale(self.ctx.session, binding)
-        count = (self.ctx.session.query(models.DVRPortBinding).
-            filter_by(port_id=binding.port_id).count())
-        self.assertFalse(count)
+        self._setup_neutron_network(network_id)
+        self._setup_neutron_port(network_id, port_id)
 
-    def test_get_dvr_port_binding_by_host_not_found(self):
-        port = ml2_db.get_dvr_port_binding_by_host(
-            self.ctx.session, 'foo_port_id', 'foo_host_id')
-        self.assertIsNone(port)
+        # Set binding result.
+        with self.ctx.session.begin(subtransactions=True):
+            result = ml2_db.set_binding_result(self.ctx.session,
+                                               port_id,
+                                               host,
+                                               vif_type)
+            self.assertEqual(port_id, result.port_id)
+            self.assertEqual(host, result.host)
+            self.assertEqual(vif_type, result.vif_type)
+            self.assertEqual(None, result.vif_details)
 
-    def test_get_dvr_port_bindings_not_found(self):
-        port = ml2_db.get_dvr_port_bindings(self.ctx.session, 'foo_port_id')
-        self.assertFalse(len(port))
+        # Get and verify binding result.
+        with self.ctx.session.begin(subtransactions=True):
+            result = ml2_db.get_binding_result(self.ctx.session,
+                                               port_id,
+                                               host)
+            self.assertEqual(port_id, result.port_id)
+            self.assertEqual(host, result.host)
+            self.assertEqual(vif_type, result.vif_type)
+            self.assertEqual('', result.vif_details)
 
-    def test_get_dvr_port_bindings(self):
-        network_id = 'foo_network_id'
-        port_id_1 = 'foo_port_id_1'
-        port_id_2 = 'foo_port_id_2'
-        self._setup_neutron_network(network_id, [port_id_1, port_id_2])
-        router = self._setup_neutron_router()
-        self._setup_dvr_binding(
-            network_id, port_id_1, router.id, 'foo_host_id_1')
-        self._setup_dvr_binding(
-            network_id, port_id_1, router.id, 'foo_host_id_2')
-        ports = ml2_db.get_dvr_port_bindings(self.ctx.session, 'foo_port_id')
-        self.assertEqual(2, len(ports))
+        # Verify binding levels.
+        with self.ctx.session.begin(subtransactions=True):
+            result_levels = ml2_db.get_binding_levels(self.ctx.session,
+                                                      port_id,
+                                                      host)
+            self.assertEqual(0, len(result_levels))
